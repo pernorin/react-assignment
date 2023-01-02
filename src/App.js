f0013d439351e75b72accec0e5090a0d3476f7f6
Per Norin <pelle.norin.79@gmail.com>
2023-01-02 23:58:52 +0100

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Form from './components/Form';
+import Filter from './components/Filter';
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header></Header>
+			<Form></Form>
+			<Filter></Filter>
+			<TodoList></TodoList>
+		</div>
+	);
 }
 
 export default App;
+
+// <form onSubmit={handleSubmit}></form>   https://www.w3schools.com/react/react_forms.asp
